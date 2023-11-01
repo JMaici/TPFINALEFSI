95f5868e3b35f28ed86078f49cf14d03f6f30b48
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-11-01 12:13:13 -0300

    keep working

--- src/components/Info.js
+++ src/components/Info.js
@@ -6,21 +6,19 @@ const Info = () => {
     <div className="container">
       <h1 className="custom-text">Información</h1>
       <p>
-        <strong>Nombre Completo:</strong> <span className="custom-link">Tu Nombre</span>
+        <strong>Nombre Completo:</strong> <span className="custom-link">Joaquin Maceira</span>
       </p>
       <p>
-        <strong>Edad:</strong> <span className="custom-link">Tu Edad</span>
+        <strong>Edad:</strong> <span className="custom-link">17</span>
       </p>
       <p>
-        <strong>Ubicación:</strong> <span className="custom-link">Tu Ubicación</span>
+        <strong>Ubicación:</strong> <a href="https://www.google.com/maps/place/Buenos+Aires" className="custom-link">Buenos Aires</a>
       </p>
       <p>
-        <strong>Correo Electrónico:</strong> <a href="mailto:tucorreo@example.com" className="custom-link">Tu Correo Electrónico</a>
+        <strong>Correo Electrónico:</strong> <a href="mailto:tucorreo@example.com" className="custom-link">example@gmail.com</a>
       </p>
       {/* Agrega más información según sea necesario */}
     </div>
   );
 };
-
 export default Info;
- 
